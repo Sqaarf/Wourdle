36c32c82ef86be47b4dc68d69b7dab7860a8bcbe
Sqaarf <mrscarf19@gmail.com>
2022-02-22 13:35:24 +0100

    Added letters cases list

--- GameUi.py
+++ GameUi.py
@@ -6,10 +6,21 @@ pygame.init()
 class GameUi:
     def __init__(self):
         self.win = pygame.display.set_mode((800, 650))
-        pygame.display.set_caption('Mario')
+        pygame.display.set_caption('Wourdle')
 
         self.fps = 30
         self.run = True
+
+        self.cases = []
+
+        # print(self.cases)
+
+        i = 0
+        for y in range(20, 550, 160):
+            self.cases.append([])
+            for x in range(20, 800, 160):
+                self.cases[i].append(pygame.Rect(x, y, 120, 120))
+            i += 1
 
     def commands(self):
         for events in pygame.event.get():
@@ -17,10 +28,9 @@ class GameUi:
                 self.run = False
 
     def render(self):
-        self.win.fill((0, 0, 0))
-        for y in range(20, 550, 160):
-            for x in range(20, 800, 160):
-                pygame.draw.rect(self.win, (255, 255, 255), (x, y, 120, 120))
+        for case in self.cases:
+            for rect in case:
+                pygame.draw.rect(self.win, (255, 255, 255), rect)
 
     def gameloop(self):
         while self.run:
@@ -31,55 +41,3 @@ class GameUi:
 
 
 pygame.quit()
-
-'''
-win = pygame.display.set_mode((600,400))
-pygame.display.set_caption('Mario')
-
-FPS = 30 #Change if you want to speed up the animation
-run = True
-
-x = 0
-y = 323
-
-def commands():
-    global run, x, y, rindex, lindex, is_right
-    keys = pygame.key.get_pressed()
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
-            run = False
-    
-    if keys[pygame.K_RIGHT]:
-        is_right = True
-        x += vel
-        rindex += 1
-    if not keys[pygame.K_RIGHT]:
-        rindex = 0
-
-        if keys[pygame.K_LEFT]:
-            is_right = False
-            x -= vel
-            lindex += 1
-        if not keys[pygame.K_LEFT]:
-            lindex = 0
-        
-def render():
-    global rindex, lindex
-    win.blit(bg,(0, 0))
-    if is_right:
-        win.blit(mario_right[rindex], (x,y))
-        if rindex >= len(mario_right) - 1:
-            rindex = 0
-    else:
-        win.blit(mario_left[lindex], (x,y))
-        if lindex >= len(mario_left) - 1:
-            lindex = 0
-    
-
-while run:
-    pygame.time.Clock().tick(FPS)
-    commands()
-    render()
-    #print(hitbox)
-    pygame.display.update()
-'''
